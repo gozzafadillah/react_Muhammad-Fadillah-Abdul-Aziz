92da5793a7e7fcf9b45e8761bdf4483f360ccbb3
gozzafadillah <gozzafadillah@gmail.com>
2022-09-02 17:06:15 +0700

    Task/Section_08: Added praktikum nama file terdiri dari jawaban soal

---
+++ 08_JavaScript_Refreshment/praktikum/Jawaban_No_1.js
@@ -0,0 +1,32 @@
+var a = 5;
+let b = "kampus merdeka";
+const nama = "Budi";
+let terdaftar = true;
+let lengkap_arr = [a, b, nama, terdaftar];
+
+let TakeArrayIndex2 = lengkap_arr[2];
+
+function perkenalan() {
+  let asal = "indonesia";
+  return console.log(
+    "perkenalkan nama saya " +
+      nama +
+      " nomor urut " +
+      a +
+      " sekarang sedang mengikuti " +
+      b +
+      " berasal dari " +
+      asal
+  );
+}
+
+if (terdaftar === true) {
+  console.log(nama + " terdaftar sebagai kegiatan kampus merdeka");
+}
+
+a = b;
+
+console.log("array = " + TakeArrayIndex2);
+console.log("a adalah = " + a);
+console.log("b adalah = " + b);
+perkenalan();
