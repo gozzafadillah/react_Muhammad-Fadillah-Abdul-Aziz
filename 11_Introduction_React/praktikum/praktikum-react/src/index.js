ba71be1ed76e612d2681459191a6cf353ffd26d8
gozzafadillah <gozzafadillah@gmail.com>
2022-09-13 10:02:17 +0700

    task/section_11: fixed and refactor name component to pascal case

--- 11_Introduction_React/praktikum/praktikum-react/src/index.js
+++ 11_Introduction_React/praktikum/praktikum-react/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
--- 11_Introduction_React/praktikum/praktikum-react/src/pages/home.js
+++ 11_Introduction_React/praktikum/praktikum-react/src/pages/home.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const home = () => {
+const Home = () => {
   return (
     <div>
       <header>
@@ -63,4 +63,4 @@ const home = () => {
   );
 };
 
-export default home;
+export default Home;
