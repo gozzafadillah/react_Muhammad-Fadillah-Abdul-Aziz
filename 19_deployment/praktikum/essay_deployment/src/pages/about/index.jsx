0ccd7819cf1faca0140d6a7f67b2462442f748ff
gozzafadillah <gozzafadillah@gmail.com>
2022-10-16 21:08:42 +0700

    task/section_19: added essay deployment

---
+++ 19_deployment/praktikum/essay_deployment/src/pages/about/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AboutPages = () => {
+  return (
+    <div>
+      <p>
+        Ini merupakan essay/praktikum mengenai deployment menggunakan Netlify
+      </p>
+    </div>
+  );
+};
+
+export default AboutPages;
